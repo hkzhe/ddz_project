d48f5c2136e91c13498b809243a6eb58dc962a81
bakey <bakey1985@qq.com>
2013-03-07 15:49:23 +0800

    s

--- game_server/server.py
+++ game_server/server.py
@@ -1,20 +1,34 @@
-import SocketServer, time  
+import SocketServer, time
+import cPickle
+import threading
+import struct
   
-class GameServer(SocketServer.BaseRequestHandler):      
-  
-    def handle(self):    
-           
-        while True:   
-            receivedData = self.request.recv(8192)   
-            if not receivedData:   
-                continue    
-        self.request.close()   
-           
+class GameServer(SocketServer.BaseRequestHandler):    
+    def handle(self):          
+        while True:
+            self.data = self.request.recv( 1024 ) 
+            if not self.data:
+                print "recv data error"
+                break
+            try:
+                msg_len = struct.unpack( "i" , self.data )
+                print msg_len
+                if  msg_len < 4 || msg_len > 10240 :
+                    print "msg len error , len = " + msg_len
+                self.data = self.request.recv( msg_len )
+                #接收剩下的数据
+                msg_body = struct.unpack( "s" , self.data );
+            except struct.error:
+                print "unpack data exception: " + self.data
+                break
+            self.request.close()           
         print 'Disconnected from', self.client_address   
-        print  
-class ThreadingServer(ThreadingMixIn, BaseHTTPServer.HTTPServer):pass  
+class ThreadingTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):pass  
 if __name__ == '__main__':
-    print 'Server is started\nwaiting for connection...\n'   
-    srvr = ThreadingServer(serveraddr, SimpleHTTPRequestHandler)
-    srvr.serve_forever()
+    print 'Server is started\nwaiting for connection...'   
+    try:
+        srvr = SocketServer.ThreadingTCPServer(  ("",8000)   , GameServer)
+        srvr.serve_forever()
+    except KeyboardInterrupt:  
+        exit() 
    
